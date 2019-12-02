253770c8b6ec7a69ceb10f4289ad760c7b0eb69c
Arif Budiman <ArifBudiman19@github.com>
2019-12-02 15:35:03 +0700

    first commit

---
+++ precache-manifest.c2daafdb472a8bf892bbda94589f4728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbab3bb4cd7f5ff0ba9581d52303c8e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b1a551d0b075c0905b6",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "2454ba5489d980769703",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0b1a551d0b075c0905b6",
+    "url": "./static/js/2.fc8a78e7.chunk.js"
+  },
+  {
+    "revision": "2454ba5489d980769703",
+    "url": "./static/js/main.b8d85de5.chunk.js"
+  },
+  {
+    "revision": "399f7fd2117afe2e6dd6",
+    "url": "./static/js/runtime-main.c114fd75.js"
+  }
+]);
